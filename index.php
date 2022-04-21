91dd423d6ae4a384b40341a8fd4e8c641f3c39ba
huypz <huyrse@gmail.com>
2022-04-21 14:00:12 -0500

    Edit home page

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ include('includes/header.html');
 <div class="page-header"><h1>Welcome</h1></div>
 <h3>Current Features</h3>
 <ul style="margin: 5px 0 0 48px;">
-    <li>Log In/Out</li>
-    <li>Register Product</li>
-    <li>View Users</li>
-    <li>View Products</li>
-    <li>Change Password</li>
+    <li>Account System</li>
+    <li>Create, Update, and Track Products & Sales</li>
+    <li>View Users & Products</li>
+    <li>Products & Sales Reports</li>
+    <li>Dynamic Shopping Cart</li>
+    <li>Check Out System</li>
 </ul>
 <br>
 <p>Please log in to view all current features. 
